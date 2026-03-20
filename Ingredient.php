ce2dd8df650bea70241151717fe76dc545c2ed89
theococquerel <theo.cocquerel2@gmail.com>
2026-03-20 10:33:36 +0100

    Classe crée

---
+++ Ingredient.php
@@ -0,0 +1,5 @@
+<?php
+class Ingredient{
+    private $nomIng;
+    private $imageIng;
+}
---
+++ Recette.php
@@ -0,0 +1,8 @@
+<?php
+class Recette{
+    private $titre;
+    private $listeIng;
+    private $describe;
+    private $photo;
+    private $tags;
+}
