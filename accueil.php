123aca92bc8ec3f89bbed7efd526bbe4bf428d5b
yannmaelbigand <yannmael.bigand@gmail.com>
2026-04-10 09:32:55 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,30 @@
+<!-- template du contenu principal de la page d'accueil -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title id="pageTitle">EZ'Cook</title>
+</head>
+<body>
+    <br><br><br><br><br><br>
+    <main id="mainContent">
+        <h3 style="font-weight: bolder; text-align: center;">Bienvenue sur le (meilleur) site de cuisine de tous les temps !</h3>
+        
+        <div id="formResearch">
+        <form method="post" id="formRes">
+            <input id="research" type="text" name="research" placeholder="  Rechercher une recette...">
+            <button id="resButton" type="button">Rechercher</button>
+        </form>
+        </div>
+        <div id="tags"></div>
+        <br>
+        <p>Ce site vous permettra de cuisiner simplement... et sans mettre le feu à la cuisine !</p><br>
+        
+        <p>Nos recettes...</p><br><br>
+        <div id="templateCook">
+
+        </div>
+        
+</body>
+</html>
